8898d48e232899890a5b376c66ac3b66680b1786
unknown <dethgrenade@gmail.com>
2020-06-02 14:41:35 -0700

    update posts working

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -52,7 +52,7 @@ router.get('/', (req, res) => {
 //Return a post in the DB by the post id
 router.get('/:id', (req, res) => {
     //Search the DB for a post matching the id
-    Posts.findById(Number(req.params.id))
+    Posts.findById(req.params.id)
         .then(post => {
             if (post) {
                 //Return the post
@@ -100,7 +100,36 @@ router.delete('/:id', (req, res) => {
 
 //Modify a post in the DB by post id
 router.put('/:id', (req, res) => {
-
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                if (req.body.title && req.body.contents) {
+                    Posts.update(req.params.id, req.body)
+                        .then(update => {
+                            if (update) {
+                                Posts.findById(req.params.id)
+                                    .then(post => {
+                                        if (post) {
+                                            //Return the post
+                                            res.status(201).json(post)
+                                        }
+                                        //Return error message Not Found
+                                        else res.status(404).json({ message: "The post with the specified ID does not exist." })
+                                    })
+                            }
+                        })
+                }
+                //Return error message Bad Request
+                else res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+            }
+            //Return error message Not Found
+            else res.status(404).json({ message: "The post with the specified ID does not exist." })
+        })
+        //Return server error message
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "The post information could not be modified." })
+        })
 })
 
 module.exports = router
